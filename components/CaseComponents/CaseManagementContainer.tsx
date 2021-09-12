040639c4df915bba50d2d93fdf1bc39a53f6457d
Prin-P <fasaipulkes@dhcp-10-250-26-214.harvard.edu>
2021-09-11 22:18:55 -0400

    fix

--- components/CaseComponents/CaseManagementContainer.tsx
+++ components/CaseComponents/CaseManagementContainer.tsx
@@ -77,7 +77,7 @@ const CaseManagementContainer: React.FC = (props) => {
               {data
               ? data.category.map((category: any, index: number) => {
                   return <Grid item xs={4} >
-                  <CaseCategory key={index} category_id={category.id} />
+                    <CaseCategory key={index} category_id={category.id} />
                   </Grid> 
                 })
               : "Something went wrong"}
